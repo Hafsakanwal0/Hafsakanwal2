ccb354e67f0ef13ebd2862ba9909dba124167755
Hafsa kanwal <125997148+Hafsakanwal2@users.noreply.github.com>
2023-05-30 00:57:11 -0700

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int area,length;
+	cout<<"enter the length of the square";
+	cin>>length;
+	area=length*length;
+	cout<<"the area of the square is"<<area;
+	return 0;
+}
